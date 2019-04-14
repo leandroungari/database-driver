0362e7955a2def60d3c4b3ba5da88d40d8297c80
Leandro Ungari <leandroungari@gmail.com>
2019-04-14 20:47:47 -0300

    Medindo tempo

---
+++ src/app/metrics.js
@@ -0,0 +1,20 @@
+export default class Metric {
+  constructor() {
+    this.timeStart = undefined;
+    this.timeEnd = undefined;
+  }
+
+  start() {
+    this.timeStart = process.hrtime();
+  }
+
+  end() {
+    this.timeEnd = process.hrtime(this.timeStart);
+  }
+
+  result() {
+    return {
+      time: this.timeEnd[1]/1000000;
+    }
+  }
+}
